e1dd6ae95faa91b397dd76d8176693d4da15a200
Lakshya Sadhwani <lakshyamwork@gmail.com>
2023-09-20 02:14:10 +0530

    added home,about and skills section

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const menuClose=document.querySelector('.nav-list li span')
 const listItems=document.querySelectorAll('.nav-list li');
 const darkModeBtn=document.getElementsByClassName('dark')[0];
 const lightModeBtn=document.getElementsByClassName('light')[0];
+const dropdown=document.querySelectorAll('.dropdown-heading');
+const dropdownList=document.querySelectorAll('.dropdown-list');
+const dropBtn=document.querySelectorAll('.btn');
 
 menuOpen.addEventListener('click',(e)=>{
     navList.classList.add('active');
@@ -20,12 +23,30 @@ listItems.forEach((item)=>{
 darkModeBtn.addEventListener('click',()=>{
     document.documentElement.style.setProperty('--bg-color','rgb(25,22,39)');
     document.documentElement.style.setProperty('--text-color','white');
+    document.documentElement.style.setProperty('--sub-text-color','white');
     lightModeBtn.classList.add('active');
     darkModeBtn.classList.remove('active');
 })
 lightModeBtn.addEventListener('click',()=>{
     document.documentElement.style.setProperty('--bg-color','white');
     document.documentElement.style.setProperty('--text-color','#242329');
+    document.documentElement.style.setProperty('--sub-text-color','#919095');
     lightModeBtn.classList.remove('active');
     darkModeBtn.classList.add('active');
+})
+dropdown.forEach((item)=>{
+    item.addEventListener('click',(e)=>{
+       if(item.classList.contains('language-btn')){
+        dropdownList[0].classList.toggle('active');
+        dropBtn[0].classList.toggle('active');
+       }
+       else if(item.classList.contains('libraries-btn')){
+        dropdownList[1].classList.toggle('active');
+        dropBtn[1].classList.toggle('active');
+       }
+       else{
+        dropdownList[2].classList.toggle('active');
+        dropBtn[2].classList.toggle('active');
+       }
+    });
 })
