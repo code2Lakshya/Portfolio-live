c6916fc03ee7d61042ee57ae29fb45e68b8323e0
Lakshya Sadhwani <lakshyamwork@gmail.com>
2023-09-24 11:43:09 +0530

    version 0 ready to deploy

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const left = getElement('.fa-chevron-left')[0];
 const right = getElement('.fa-chevron-right')[0];
 let c=0;
 const slideBtn=getElement('.project-footer span');
+const scroller=getElement('.scroller')[0];
 
 qualificationBtn.forEach((item) => {
     item.addEventListener('click', (e) => {
@@ -63,14 +64,16 @@ listItems.forEach((item) => {
 darkModeBtn.addEventListener('click', () => {
     document.documentElement.style.setProperty('--bg-color', 'rgb(25,22,39)');
     document.documentElement.style.setProperty('--text-color', 'white');
-    document.documentElement.style.setProperty('--sub-text-color', 'white');
+    document.documentElement.style.setProperty('--form-bg-color', '#211D35');
+    document.documentElement.style.setProperty('--footer-bg-color', '#6E57E0');
     lightModeBtn.classList.add('active');
     darkModeBtn.classList.remove('active');
 })
 lightModeBtn.addEventListener('click', () => {
     document.documentElement.style.setProperty('--bg-color', 'white');
     document.documentElement.style.setProperty('--text-color', '#242329');
-    document.documentElement.style.setProperty('--sub-text-color', '#919095');
+    document.documentElement.style.setProperty('--form-bg-color', '#F0EEFC');
+    document.documentElement.style.setProperty('--footer-bg-color', 'white');
     lightModeBtn.classList.remove('active');
     darkModeBtn.classList.add('active');
 })
@@ -143,3 +146,11 @@ slideBtn.forEach(item=>{
     })
 })
 setInterval(rightSlide,7000);
+
+window.addEventListener('scroll',()=>{
+    console.log(window.scrollY);
+    window.scrollY>540 ? scroller.classList.add('active'): scroller.classList.remove('active');
+})
+scroller.addEventListener('click',()=>{
+    window.scrollTo(0,0);
+})
